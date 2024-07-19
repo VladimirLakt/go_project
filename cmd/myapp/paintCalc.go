ef886a0237561cf125fae68d4713687e01d9bfa0
VladimirLakt <lact.v@yandex.ru>
2024-07-19 14:42:03 +0700

    19-07-2024 программа для расчета краски с учётом вывода ошибки при отрицательном значении

--- cmd/myapp/paintCalc.go
+++ cmd/myapp/paintCalc.go
@@ -1,25 +1,33 @@
-// игра - в которой необходимо угадать число за 10 попыток
+// расчет необходимой краски
 package main
 
 import (
 	"fmt"
+	"log"
 )
 
-func paintNeeded(width, height float64) float64 {
-
+func paintNeeded(width, height float64) (float64, error) {
+	if width < 0 || height < 0 {
+		return 0, fmt.Errorf("Длина или ширина не могут быть меньше нуля ")
+	}
 	area := width * height
-	return area / 10
+	return area / 10, nil
 }
 
 func main() {
 
 	var amount, total float64
-	amount = paintNeeded(4.2, 3.0)
+	amount, err := paintNeeded(4.2, 3.0)
+	if err != nil {
+		log.Fatal(err)
+	}
 	fmt.Printf("%.2f liters needed\n", amount)
 	total += amount
-	amount = paintNeeded(4.2, 3.0)
+	amount, err = paintNeeded(4.2, 3.0)
+	if err != nil {
+		log.Fatal(err)
+	}
 	fmt.Printf("%.2f liters needed\n", amount)
 	total += amount
 	fmt.Printf("Total: %.2f liters\n", total)
-
 }
